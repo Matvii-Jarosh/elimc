6cdd3e402658026ce85d8760ac0776aa49ed8adf
Matvii Jarosh <164613910+Matvii-Jarosh@users.noreply.github.com>
2025-02-19 09:39:46 +0200

    Add files via upload

--- elimc.el
+++ elimc.el
@@ -1,9 +1,10 @@
-;;; elimc.el --- Emacs lisp implementation of math calculate
+;;; elimc.el --- Emacs lisp implementation of math calculate -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2025 Matvii Jarosh
 
 ;; Author: Matvii Jarosh <matviijarosh@gmail.com>
 ;; Created: 2025-02-15
+;; Version: 1.0
 
 ;; elimc is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,27 +25,34 @@
 ;; mathematical problems using infix and rpn
 
 ;; to get started with simple calculator call elimc-calc
-;; if you want plot call elimc-plo
+;; if you want plot call elimc-plot
 
 ;;; Code:
+
+(defvar nan-value -0.0e+NaN)
+(defvar expr-widget)
+(defvar x-val-widget)
+(defvar result-widget)
+(defvar error-widget)
+(defvar notation-widget)
 
 (require 'widget)
 
 ;; utils
 (defun elimc-token-operatorp (token)
-  "Return t if token is a operator"
+  "Return t if TOKEN is a operator."
   (if (stringp token)
       (member token '("+" "-" "*" "/" "^"))
     (error "Token is not a string")))
 
 (defun elimc-token-funp (token)
-  "Return t if token is a math function"
+  "Return t if TOKEN is a math function."
   (if (stringp token)
       (member token '("sin" "cos" "tg" "ctg" "sqrt" "abs"))
     (error "Token is not a string")))
 
 (defun elimc-op-preced (token)
-  "Returns the operator precedence"
+  "Return the operator precedence based on TOKEN."
   (if (stringp token)
       (cond
        ((string-equal token "^") 4)
@@ -54,15 +62,15 @@
     (error "Token is not a string")))
 
 (defun elimc-tokenize-math-expression (expr)
-  "Splits a mathematical expression into a list of tokens."
+  "Splits a mathematical EXPR into a list of tokens."
   (if (stringp expr)
       (let ((expr (replace-regexp-in-string "\\([-+*/^()]\\)" " \\1 " expr)))
 	(split-string (string-trim expr)))
     (error "Expr is not a string")))
 
-;; shunting-yard 
+;; shunting-yard
 (defun elimc-shunting-yard (input)
-  "Converts a mathematical expression to Reverse Polish Notation (RPN)"
+  "Convert a mathematical expression to Reverse Polish Notation from INPUT."
   (if (listp input)
       (let ((tokens (copy-tree input))
             (stack nil)
@@ -71,7 +79,7 @@
 	  (let ((token (pop tokens)))
             (cond
              ((string-match "^[0-9.]+$" token) ;; is number
-              (push token output)) 
+              (push token output))
              ((elimc-token-funp token) ;; is function
               (push token stack))
              ((elimc-token-operatorp token) ;; is operator
@@ -100,47 +108,69 @@
 	(nreverse output))
     (error "The input argument is not a list")))
 
-;; rpn
+;; rpnq
+(defun elimc-rpn-fun (stack token)
+  "Handles operations in the RPN.
+
+STACK is a main RPN stack
+
+TOKEN is a function
+
+Returns the result of the operation, or NaN if invalid."
+  (if (null stack)
+      (list nan-value)
+    (let* ((a (car stack))
+           (new-stack (cdr stack)))
+      (if (equal a nan-value)
+          (cons nan-value new-stack)
+        (let ((result (condition-case nil
+                          (cond ((string-equal token "sin") (sin a))
+                                ((string-equal token "cos") (cos a))
+                                ((string-equal token "tg") (tan a))
+                                ((string-equal token "ctg") (/ 1.0 (tan a)))
+                                ((string-equal token "sqrt")
+                                 (if (< a 0) nan-value (sqrt a)))
+                                ((string-equal token "abs") (abs a)))
+                        (error nan-value))))
+          (cons result new-stack))))))
+
+(defun elimc-rpn-oper (a b token)
+  "Handles operations in the RPN.
+
+A and B is values from stack
+
+TOKEN is a operator
+
+Returns the result of the operation, or NaN if invalid."
+  (if (or (equal a nan-value) (equal b nan-value))
+      nan-value
+    (condition-case nil
+        (cond ((string-equal token "+") (+ a b))
+              ((string-equal token "-") (- a b))
+              ((string-equal token "*") (* a b))
+              ((string-equal token "/") (if (zerop b) nan-value (/ a b)))
+              ((string-equal token "^") (expt a b)))
+      (error nan-value))))
+
 (defun elimc-rpn (input x-val)
-  "Calculates the value of an expression in RPN. Returns NaN if invalid."
+  "Calculates the value of an INPUT and X-VAL in RPN.
+
+Return NaN if invalid."
   (if (and (listp input) (numberp x-val))
       (let ((tokens (copy-tree input))
-            (stack nil)
-            (nan-value 0.0e+NaN))
+            (stack nil))
         (while (and tokens (not (equal (car stack) nan-value)))
           (let ((token (pop tokens)))
-            (condition-case err
+            (condition-case nil
                 (cond
                  ((string-match "^[0-9.]+$" token)
                   (push (float (string-to-number token)) stack))
                  ((elimc-token-funp token)
-                  (let ((a (pop stack)))
-                    (if (equal a nan-value)
-                        (push nan-value stack)
-                      (let ((result (condition-case nil
-                                        (cond ((string-equal token "sin") (sin a))
-                                              ((string-equal token "cos") (cos a))
-                                              ((string-equal token "tg") (tan a))
-                                              ((string-equal token "ctg") (/ 1.0 (tan a)))
-                                              ((string-equal token "sqrt")
-                                               (if (< a 0) nan-value (sqrt a)))
-                                              ((string-equal token "abs") (abs a)))
-                                      (error nan-value))))
-                        (push result stack)))))
+                  (setq stack (elimc-rpn-fun stack token)))
                  ((elimc-token-operatorp token)
                   (let ((b (pop stack))
                         (a (pop stack)))
-                    (if (or (equal a nan-value) (equal b nan-value))
-                        (push nan-value stack)
-                      (let ((result (condition-case nil
-                                        (cond ((string-equal token "+") (+ a b))
-                                              ((string-equal token "-") (- a b))
-                                              ((string-equal token "*") (* a b))
-                                              ((string-equal token "/")
-                                               (if (zerop b) nan-value (/ a b)))
-                                              ((string-equal token "^") (expt a b)))
-                                      (error nan-value))))
-                        (push result stack)))))
+                    (push (elimc-rpn-oper a b token) stack)))
                  ((member token '("x" "pi" "e"))
                   (push (float (cond ((string-equal token "x") x-val)
                                      ((string-equal token "pi") float-pi)
@@ -152,19 +182,16 @@
           (pop stack)))
     (error "Invalid input or x-val")))
 
-
-
 ;; tui
 (eval-when-compile
   (require 'wid-edit))
 
-(defvar expr-widget)
-(defvar x-val-widget)
-(defvar result-widget)
-(defvar error-widget)
-(defvar notation-widget)
-
 (defmacro elimc-in-widget (buffer-name &rest body)
+  "Macro for quickly building a widget base.
+
+BUFFER-NAME is the name of the buffer in which the program will be executed.
+
+BODY is the content of the application with all the code."
   `(progn
      (switch-to-buffer ,buffer-name)
      (kill-all-local-variables)
@@ -180,14 +207,29 @@
      (use-local-map widget-keymap)
      (widget-setup)))
 
+(defun elimc-calc-calculate (expr notation x-val result-widget-local error-widget-local)
+  "Calculate the RESULT based on the EXPR, NOTATION, and X-VAL.
+Then update the RESULT-WIDGET-LOCAL and ERROR-WIDGET-LOCAL."
+  (condition-case err
+      (let* ((tokens (elimc-tokenize-math-expression expr))
+             (result (if (string= notation "Infix")
+                         (elimc-rpn (elimc-shunting-yard tokens) x-val)
+                       (elimc-rpn tokens x-val))))
+        (widget-value-set result-widget-local (format "%s" result))
+        (widget-value-set error-widget-local ""))
+    (error
+     (widget-value-set result-widget-local "")
+     (widget-value-set error-widget-local (error-message-string err)))))
+
+
 (defun elimc-calc ()
-  "Client for calculating values "
+  "Client for calculating values."
   (interactive)
   (elimc-in-widget "*RPN calculator*"
 		   (setq expr-widget
 			 (widget-create 'editable-field
 					:format "RPN expr:\n%v"
-					"2 2 +"))
+					"2 + 2"))
 		   (widget-insert "\n")
 		   (setq x-val-widget
 			 (widget-create 'editable-field
@@ -201,59 +243,38 @@
 		   (setq error-widget (widget-create 'item :value ""))
 		   (widget-insert "\n")
 		   (setq notation-widget (widget-create 'radio-button-choice
-				  :value "Infix"
-				  '(item "Infix") '(item "RPN")))
+							:value "Infix"
+							'(item "Infix") '(item "RPN")))
 		   (widget-create 'push-button
 				  :notify
 				  (lambda (&rest ignore)
-				    (condition-case err
-					(let*
-					    ((expr
-					      (widget-value
-					       expr-widget))
-					     (notation
-					      (widget-value notation-widget))
-					     (tokens
-					      (elimc-tokenize-math-expression
-					       expr))
-					     (x-val
-					      (string-to-number
-					       (widget-value x-val-widget)))
-					     (result
-					      (if
-						  (string= notation "Infix")
-                                                  (elimc-rpn
-						   (elimc-shunting-yard tokens)
-						   x-val)
-                                                (elimc-rpn tokens x-val))))
-					  (widget-value-set
-					   result-widget
-					   (format "%s" result))
-					  (widget-value-set error-widget ""))
-				      (error 
-				       (widget-value-set result-widget "")
-				       (widget-value-set
-					error-widget
-					(error-message-string err))))
-				    (widget-setup))
+				    (ignore ignore)
+				    (elimc-calc-calculate
+				     (widget-value expr-widget)
+				     (widget-value notation-widget)
+				     (string-to-number (widget-value x-val-widget))
+				     result-widget
+				     error-widget))
 				  "Calculate")))
 
 ;; ui graphic
 (defun elimc-create-image (width height)
-  "Creates an image as a list, filling it with white (255)"
+  "Create an image as a list, filling it with white (255).
+
+WIDTH and HEIGHT this is the size of the image."
   (let ((default-color 255))
     (make-list height (make-list width default-color))))
 
 (defun elimc-fill-image (image color)
-  "Fills all pixels of the image IMAGE with the specified color COLOR"
+  "Fills all pixels of the image IMAGE with the specified color COLOR."
   (mapcar (lambda (row) (make-list (length row) color)) image))
 
 (defun elimc-setpixel (image x y color)
-  "Sets the pixel in the image IMAGE at position (x, y) to the color COLOR"
+  "Set the pixel in the IMAGE at position (X, Y) to the COLOR."
   (setf (nth x (nth y image)) color))
 
 (defun elimc-image-to-pbm (image)
-  "Converts an IMAGE image to a PBM string representation"
+  "Convert an IMAGE to a PBM string representation."
   (let* ((width (length (car image)))
          (height (length image))
          (pbm-data (list (format "%d %d" width height)
@@ -267,13 +288,50 @@
     (string-join (reverse pbm-data) "\n")))
 
 (defun elimc--push-image (image)
-  "Creates a new buffer and inserts the image into it"
+  "Create a new buffer and insert the IMAGE into it."
   (with-help-window "*Plot image*"
     (with-current-buffer "*Plot image*"
       (insert-image image))))
 
+(defun elimc-plot-create-graph (expr notation error-widget-local)
+  "Create a plot for the given EXPR in the specified NOTATION.
+Then update the ERROR-WIDGET-LOCAL."
+  (condition-case err
+      (let*
+          ((tokens (elimc-tokenize-math-expression expr))
+           (rpn (if (string= notation "Infix")
+                    (elimc-shunting-yard tokens)
+                  tokens))
+           (image (elimc-fill-image (elimc-create-image 400 400) 0)))
+
+        ;; grid
+        (dotimes (x 400)
+          (dotimes (y 400)
+            (when (or (= (mod x 50) 0)
+                      (= (mod y 50) 0))
+              (elimc-setpixel image x y 1))))
+
+        ;; plot
+        (dotimes (x 400)
+          (let* ((x-val (- (/ (* x 8.0) 400) 4))
+                 (y-val (elimc-rpn rpn x-val)))
+            (if (or (isnan y-val) (> (abs y-val) 1.0e6))
+                (setq y-val 0.0e+NaN)
+              (let ((y (round (- 200 (* 50 y-val)))))
+                (when (and (>= y 0) (< y 400))
+                  (elimc-setpixel image x y 1))))))
+
+        ;; display img
+        (let ((pbm (elimc-image-to-pbm image)))
+          (with-current-buffer "*Function Plotter*"
+            (let ((inhibit-read-only t))
+              (goto-char (point-max))
+              (elimc--push-image (create-image pbm 'pbm ""))))))
+    (error
+     (widget-value-set error-widget-local (error-message-string err)))))
+
 (defun elimc-plot ()
-  "Client for drawing plot mathematics"
+  "Client for drawing plot mathematics."
   (interactive)
   (elimc-in-widget "*Function Plotter*"
 		   (setq expr-widget
@@ -285,69 +343,17 @@
 		   (setq error-widget (widget-create 'item :value ""))
 		   (widget-insert "\n")
 		   (setq notation-widget (widget-create 'radio-button-choice
-				  :value "Infix"
-				  '(item "Infix") '(item "RPN")))
+							:value "Infix"
+							'(item "Infix") '(item "RPN")))
 		   (widget-insert "\n")
 		   (widget-create 'push-button
 				  :notify
 				  (lambda (&rest ignore)
-				    (condition-case err
-					(let*
-					    ((expr (widget-value expr-widget))
-					     (notation
-					      (widget-value notation-widget))
-					     (tokens
-					      (elimc-tokenize-math-expression
-					       expr))
-					     (rpn
-					      (if
-						  (string= notation "Infix")                                                  
-						  (elimc-shunting-yard tokens)
-                                                 tokens)) 
-					     (image
-					      (elimc-fill-image
-					       (elimc-create-image 400 400)
-					       0)))
-
-					  ;; grid
-					  (dotimes (x 400)
-					    (dotimes (y 400)
-					      (when (or (= (mod x 50) 0)
-							(= (mod y 50) 0))
-						(elimc-setpixel image x y 1))))
-					  
-					  ;; plot
-					  (dotimes (x 400)
-					    (let* ((x-val
-						    (- (/ (* x 8.0) 400) 4))
-						   (y-val
-						    (elimc-rpn rpn x-val)))
-					      (if (or (isnan y-val)
-						      (> (abs y-val) 1.0e6))
-						  (setq y-val 0.0e+NaN)
-						(let ((y (round
-							  (- 200
-							     (* 50 y-val)))))
-						  (when (and (>= y 0)
-							     (< y 400))
-						    (elimc-setpixel
-						     image x y 1))))))
-					  
-					  ;; display img
-					  (let
-					      ((pbm
-						(elimc-image-to-pbm image)))
-					    (with-current-buffer 
-						"*Function Plotter*"
-					      (let ((inhibit-read-only t))
-						(goto-char (point-max))
-						(elimc--push-image
-						 (create-image
-						  pbm 'pbm ""))))))
-				      (error 
-				       (widget-value-set
-					error-widget
-					(error-message-string err)))))
+				    (ignore ignore)
+				    (elimc-plot-create-graph
+				     (widget-value expr-widget)
+				     (widget-value notation-widget)
+				     error-widget))
 				  "Plot")))
 
 ;;; elimc.el ends here
